41bf03fbf8632e09bffc15c604ff46144f85c515
lukhwa <lukhwa@gmail.com>
2011-10-14 01:50:00 +0700

    Add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
@@ -6,6 +8,7 @@ SampleApp::Application.routes.draw do
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
   
   root :to => 'pages#home'
  
